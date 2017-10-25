820b9af993677fa0fc346472760ca1717a3a56cb
Mikhalkov, Eugene <EMikhalkov@qulix.com>
2017-10-25 16:28:42 +0300

    setup network manager

--- News reader/Core/Constants.swift
+++ News reader/Core/Constants.swift
@@ -2,4 +2,6 @@ import Foundation
 
 struct Constants {
     static let cellIdentifier = "NewsCell"
+    static let apiKey = "da81ed3b203b4dc5901390438f6f30a6"
+    static let url = "https://newsapi.org/v1/articles"
 }
--- News reader/Model/NetworkManager.swift
+++ News reader/Model/NetworkManager.swift
@@ -1,1 +1,48 @@
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+public enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+}
+
+public class NetworkManager {
+
+    typealias NewsCompletion = (Result<[NewsModel]>) -> Void
+
+    func fetchTrendedGifs(source: String, sortBy: String, completionHandler: @escaping NewsCompletion) {
+
+        request(url: Constants.url, parameters: getParameters(source: source, sortBy: sortBy)) { result -> Void in
+            completionHandler(result)
+        }
+    }
+
+    private func getParameters(source: String, sortBy: String) -> [String: Any] {
+
+        let parameters: [String: Any] = [
+            "source": source,
+            "sortBy": sortBy,
+            "apiKey": Constants.apiKey
+        ]
+        return parameters
+    }
+
+    private func request(url: String, parameters: [String: Any], completion: @escaping NewsCompletion) {
+        
+        Alamofire.request(url,
+                          method: .get,
+                          parameters: parameters,
+                          encoding: URLEncoding.default).responseArray(keyPath: "articles") { (response: DataResponse<[NewsModel]>) in
+
+                            switch response.result {
+
+                            case .failure(let error):
+                                completion(.failure(error))
+                            case .success(let value):
+                                completion(.success(value))
+                            }
+        }
+    }
+    
+}
