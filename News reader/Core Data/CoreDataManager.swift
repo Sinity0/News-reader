bdf88613b7e6f2c5049d6b94dbfb5909a10b330a
Mikhalkov, Eugene <EMikhalkov@qulix.com>
2017-10-26 14:25:09 +0300

    custom error handling; CoreData setup

---
+++ News reader/Core Data/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  News reader
+//
+//  Created by Mikhalkov, Eugene on 10/26/17.
+//  Copyright © 2017 Mikhalkov, Eugene. All rights reserved.
+//
+
+import Foundation
--- News reader/Core/Utils.swift
+++ News reader/Core/Utils.swift
@@ -1,8 +1,20 @@
 import UIKit
 
+public struct CustomError: LocalizedError {
+    var title: String
+    var description: String? { return _description }
+
+    private var _description: String
+
+    init(title: String?, description: String) {
+        self.title = title ?? "Error"
+        self._description = description
+    }
+}
+
 extension UIViewController {
-    func showAlert(_ message: String) -> UIAlertController {
-        let alertController = UIAlertController(title: "Gifer", message: message, preferredStyle: .alert)
+    func showAlert(title:String, message: String) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let confirm = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alertController.addAction(confirm)
         return alertController
